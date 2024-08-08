6c260f6f567cf3b9d5eedb49d80e98fb7996424e
Anthony Boschat <anthonyboschat.dev@hotmail.com>
2024-08-08 16:11:14 +0200

    Initialisation graphQL dans le projet react

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,17 @@ import App from './App.jsx'
 import { Provider } from 'react-redux'
 import { store } from './Redux/store.js'
 import { BrowserRouter } from 'react-router-dom'
+import client from './appollo-client.js'
+import { ApolloProvider } from '@apollo/client'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+    <ApolloProvider client={client}>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </ApolloProvider>
   </React.StrictMode>,
 )
